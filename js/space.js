1fd9da96eb12157038ed4a356e24cbac6ae21136
nachoperez101 <ignacio.perezsilva11@gmail.com>
2022-09-10 16:06:42 -0300

    actualizacion
    muestra todo medio roto, pero muestra cosas

--- js/space.js
+++ js/space.js
@@ -1,34 +1,41 @@
 let inputBuscar = document.getElementById("inputBuscar");
-// const input_value = inputBuscar.value;
-const images_nasa = await fetch(`https://images-api.nasa.gov/search?q=andromeda`)
-.then(response => response.json());
-// console.log(images_nasa.collection.items[0].href)
-let imagen = images_nasa.collection.items[0].href
+let images_nasa;
 
 
 // BOTONES
 let btnBuscar = document.getElementById("btnBuscar");
 const div_contenedor = document.getElementById("contenedor");
 
-const imagen_de_nasa = await fetch(`${imagen}`)
-.then(response => response.json());
-console.log(imagen_de_nasa[0])
 
 
-function search() {
-    
-}
+btnBuscar.addEventListener("click", async function(){
+  if (inputBuscar.value != ''){
+    images_nasa = await fetch(`https://images-api.nasa.gov/search?q=${inputBuscar.value}`).then(response => response.json());
+    show(images_nasa.collection.items);
+  }
+  else{
+    show([]);
+  }
+})
 
-// btnBuscar.addEventListener("click", search)
 
-// search()
+function show(items) {
 
-/* div_contenedor.innerHTML = `<div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="${imagen_de_nasa[0]}" alt="Card image cap">
+  let contentToAppend = '';
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].links == undefined) continue;
+    contentToAppend += `
+    <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="${items[i].links.href}" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
-  </div>` */
+    </div>`
+  }
+
+  div_contenedor.innerHTML = contentToAppend;
+}
+
 
