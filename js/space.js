4ee7afd6425a8dc49e54e6c02313d1ba6e1d3ac1
nachoperez101 <ignacio.perezsilva11@gmail.com>
2022-09-15 09:16:18 -0300

    grupal terminado

--- js/space.js
+++ js/space.js
@@ -25,13 +25,18 @@ function show(items) {
   for (let i = 0; i < items.length; i++) {
     if (items[i].links == undefined) continue;
     contentToAppend += `
-    <div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="${items[i].links.href}" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
+    <div class="col">
+      <div class="card" style="width: 25rem; height: 30rem; margin-top: 1rem
+      ">
+      <img class="card-img-top" src="${items[i].links[0].href}" alt="Card image cap">
+      <div class="card-body" style="overflow: auto">
+        <h5 class="card-title">${items[i].data[0].title}</h5>
+        <p class="card-text">${items[i].data[0].description}</p>
+      </div>
+      <div class="card-footer">
+        <small>${items[i].data[0].date_created}</small>
+      </div>
+      </div>
     </div>`
   }
 
